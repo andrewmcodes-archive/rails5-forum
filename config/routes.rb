3b33f93be9e9926f58ba8113b9bcd9178967f40f
Andrew Mason <astrosurfer888@gmail.com>
2018-02-27 11:05:19 -0500

    Update basic styles

--- config/routes.rb
+++ config/routes.rb
@@ -12,6 +12,7 @@
 # 
 
 Rails.application.routes.draw do
+  root to: 'discussions#index'
   resources :discussions
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
